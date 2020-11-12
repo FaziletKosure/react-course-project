f429624fb541fc0e1906b8e5b28e70ae4e03f933
FaziletKosure <faziletkosure1@gmail.com>
2020-11-12 16:02:50 +0100

    project ended

--- movies_app_react/src/App.js
+++ movies_app_react/src/App.js
@@ -1,12 +1,12 @@
 import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 import Movies from './components/Movies'
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   const FEATURED_API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
-  const IMG_API="https://image.tmdb.org/t/p/w1280"
+  // const IMG_API="https://image.tmdb.org/t/p/w1280"
   const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
   // const apiKey='http://www.omdbapi.com/?apikey=fb9a766c&s=iron&page=20'
@@ -26,10 +26,8 @@ const [searchTerm, setSearchTerm]=useState('')
   const handleOnSubmit=(e)=>{
     e.preventDefault();
 
-    if(searchTerm){ 
-       fetchMovies(SEARCH_API + searchTerm); 
-        setSearchTerm('');
-      }
+    searchTerm ? fetchMovies(SEARCH_API + searchTerm) : setSearchTerm('');
+      
   }
 
   const handleOnChange=(e)=>{
