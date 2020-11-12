294a18cd25f84adc6eb82a6e21819b121db42b94
FaziletKosure <faziletkosure1@gmail.com>
2020-11-12 13:42:49 +0100

    add movies component

--- movies_app_react/src/App.js
+++ movies_app_react/src/App.js
@@ -1,23 +1,39 @@
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
+import Movies from './components/Movies'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const FEATURED_API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+  const IMG_API="https://image.tmdb.org/t/p/w1280"
+  const SEARCH_API="https://api.themoviedb.org/3//search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+
+  // const apiKey='http://www.omdbapi.com/?apikey=fb9a766c&s=iron&page=20'
+  const [movies, setMovies] = useState([])
+
+ 
+  const fetchMovies=async()=>{
+   const {data}= await axios.get( FEATURED_API)
+      console.log(data.results);
+      setMovies(data.results)
+      console.log(movies);
+    
+     
+    
+  }
+
+  useEffect(() => {
+  fetchMovies()
+  }, [])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="movie-container">
+
+      {movies.map(movie=>
+        <Movies key={movie.id} {...movie}/>)}
+     
     </div>
   );
 }
--- movies_app_react/src/components/Movies.js
+++ movies_app_react/src/components/Movies.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
-export default function Movies() {
+const IMG_API="https://image.tmdb.org/t/p/w1280"
+export default function Movies({title,vote_average,poster_path,overview}) {
+    
     return (
-        <div>
-           Movies Fazilet
+        <div className="movie">
+            <img src={IMG_API + poster_path} alt={title}/>
+            <div className="movie-info">
+                <h3>{title}</h3>
+    <span>{vote_average}</span>
+            </div>
+            <div className="movie-over">
+                <h2>Overview:</h2>
+    <p>{overview}</p>
+            </div>
+          
         </div>
     )
 }
