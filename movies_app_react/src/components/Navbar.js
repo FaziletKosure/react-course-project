0a9cf5832fba7fe49e34b83db6fd145981cfa199
FaziletKosure <faziletkosure1@gmail.com>
2020-11-14 23:27:25 +0100

    add

---
+++ movies_app_react/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+         <ul>
+             <li>Home</li>
+             <li>About</li>
+             <li>Movies</li>
+        </ul>   
+        </div>
+    )
+}
+
+export default Navbar
