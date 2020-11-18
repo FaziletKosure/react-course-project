625dda384d51485214c0ea724a0b6bddfebbeeca
FaziletKosure <faziletkosure1@gmail.com>
2020-11-18 23:04:55 +0100

    updated

--- react_website/src/compnents/pages/Products.js
+++ react_website/src/compnents/pages/Products.js
@@ -4,22 +4,16 @@ import Cards from '../Cards';
 import Footer from '../Footer'
 
 
-const videoStyle = {  
-  // objectFit:" cover",
-  // marginBottom: "13%"
- };
+
 
 export default function Products() {
 
   return (
-     <div>
-{/* <div style={videoStyle}>
-      <video className="videocontainer" src='/videos/this-is-clarusway.mp4' autoPlay loop/>
-      </div>  */}
-     <div>
+    <>
      <Cards />
      <Footer />
-     </div>
- 
-  </div>)
+     </>
+    
+  )
+  
 }
