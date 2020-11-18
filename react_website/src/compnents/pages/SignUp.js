acbb788ead026ad2633e31242c4be0d395ba9755
FaziletKosure <faziletkosure1@gmail.com>
2020-11-18 19:21:34 +0100

    updated

--- react_website/src/compnents/pages/SignUp.js
+++ react_website/src/compnents/pages/SignUp.js
@@ -4,9 +4,9 @@ import Form from '../../Form/Form'
 
 export default function SignUp() {
   return (
-    <>
+    <div className="sign-up-container">
 <Form/>
   <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>
-  </>
+  </div>
   );
 }
