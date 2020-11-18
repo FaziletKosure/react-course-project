544d232499f26974a1d24e99557d84bcd13fe240
FaziletKosure <faziletkosure1@gmail.com>
2020-11-18 22:43:38 +0100

    updated

--- react_website/src/compnents/pages/SignUp.js
+++ react_website/src/compnents/pages/SignUp.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../../App.css';
 import Form from '../../Form/Form'
+import Footer from '../Footer'
 
 export default function SignUp() {
   return (
+    <>
     <div className="sign-up-container">
-<Form/>
-<img src="/images/design-min-1-1.jpg" alt="" width="40%" height="500vh"/>
-  {/* <h1 className='sign-up'jpg></h1> */}
+      <Form/>
+      <img src="/images/design-min-1-1.jpg" alt="" width="40%" height="500vh"/>
   </div>
+  <Footer />
+  </>
   );
 }
