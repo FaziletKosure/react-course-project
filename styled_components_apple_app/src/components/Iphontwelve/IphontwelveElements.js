14a477d6fd1896372024eea4ebab61f6f1029c76
FaziletKosure <faziletkosure1@gmail.com>
2020-11-23 20:44:26 +0100

    updated

--- styled_components_apple_app/src/components/Iphontwelve/IphontwelveElements.js
+++ styled_components_apple_app/src/components/Iphontwelve/IphontwelveElements.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-// import ImgBg from '../../images/pizza-3.jpg';
-// import ImgBg from '../../images/Scottsdale.jpg';
+
 
 export const Iphonvideo = styled.video`
 object-fit: cover;
@@ -10,61 +9,3 @@ position: fixed;
 z-index: -1;
 `;
 
-// export const HeroContent = styled.div`
-//   height: calc(100vh - 80px);
-//   max-height: 100%;
-//   padding: 0rem calc((100vw - 1300px) / 2);
-// `;
-
-// export const HeroItems = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: flex-start;
-//   height: 100vh;
-//   max-height: 100%;
-//   padding: 0 2rem;
-//   width: 650px;
-//   color: #fff;
-//   text-transform: uppercase;
-//   line-height: 1;
-//   font-weight: bold;
-
-//   @media screen and (max-width: 650px) {
-//     width: 100%;
-//   }
-// `;
-
-// export const HeroH1 = styled.h1`
-//   font-size: clamp(2.5rem, 10vw, 5rem);
-//   margin-bottom: 1rem;
-//   box-shadow: 3px 5px #e9ba23;
-//   letter-spacing: 3px;
-// `;
-
-// export const HeroP = styled.p`
-//   font-size: clamp(2rem, 2.5vw, 3rem);
-//   margin-bottom: 2rem;
-// `;
-
-// export const HeroBtn = styled.button`
-//   font-size: 1.4rem;
-//   padding: 1rem 4rem;
-//   border: none;
-//   border-radius: 2px;
-//   // background: #12005e;
-//   background: linear-gradient(
-//     90deg,
-//     rgb(39, 143, 255) 20%,
-//     rgb(12, 99, 250) 100%
-//   );
-//   color: #fff;
-//   transition: 0.2s ease-out;
-
-//   &:hover {
-//     background: #ffc500;
-//     transition: 0.2s ease-out;
-//     cursor: pointer;
-//     color: #000;
-//   }
-// `;
